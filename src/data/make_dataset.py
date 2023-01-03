8d61ddfad9cdb7635b7ec12ce3c37bd3780e4365
EVDIO <evangelos.dio@gmail.com>
2023-01-03 16:24:56 +0100

    Fix make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,10 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+import numpy as np
+import torch
+import os
+from torchvision import transforms
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -12,6 +16,32 @@ def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    # initialize lists to hold the training and test data
+    train_data = []
+    test_data = []
+    
+    # load the training data
+    for i in range(8):
+        npz_file = np.load(os.path.join(input_filepath, 'train_{}.npz'.format(i)))
+        data = npz_file['images']
+        train_data.append(data)
+    
+    # load the test data
+    npz_file = np.load(os.path.join(input_filepath, 'test.npz'))
+    test_data = npz_file['images']
+    
+    # concatenate the training data into a single array
+    train_data = np.concatenate(train_data)
+    
+    # apply normalization transform to the data
+    normalize_transform = transforms.Normalize((0.5,), (0.5,))
+    train_data = normalize_transform(torch.from_numpy(train_data))
+    test_data = normalize_transform(torch.from_numpy(test_data))
+    
+    # save the normalized data to the save directory
+    torch.save(train_data, os.path.join(output_filepath, 'train_data.pt'))
+    torch.save(test_data, os.path.join(output_filepath, 'test_data.pt'))
+
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 

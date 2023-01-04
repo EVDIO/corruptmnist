40d96a3a41b8fb461e3bc2fff59b05754f288488
EVDIO <evangelos.dio@gmail.com>
2023-01-04 16:34:47 +0100

    upd 1/4/2023 16:34

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -16,35 +16,47 @@ def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+
+    logger = logging.getLogger(__name__)
+    logger.info('making final data set from raw data')
+
     # initialize lists to hold the training and test data
     train_data = []
+    train_labels = []
     test_data = []
+    test_labels = []
     
     # load the training data
     for i in range(8):
         npz_file = np.load(os.path.join(input_filepath, 'train_{}.npz'.format(i)))
         data = npz_file['images']
+        labels = npz_file['labels']
         train_data.append(data)
+        train_labels.append(labels)
     
     # load the test data
     npz_file = np.load(os.path.join(input_filepath, 'test.npz'))
     test_data = npz_file['images']
-    
-    # concatenate the training data into a single array
+    test_labels = npz_file['labels']
+
+    # concatenate the training data and labels into single arrays
     train_data = np.concatenate(train_data)
+    train_labels = np.concatenate(train_labels)
     
     # apply normalization transform to the data
     normalize_transform = transforms.Normalize((0.5,), (0.5,))
     train_data = normalize_transform(torch.from_numpy(train_data))
     test_data = normalize_transform(torch.from_numpy(test_data))
     
-    # save the normalized data to the save directory
-    torch.save(train_data, os.path.join(output_filepath, 'train_data.pt'))
-    torch.save(test_data, os.path.join(output_filepath, 'test_data.pt'))
+    # convert labels to PyTorch tensors
+    train_labels = torch.from_numpy(train_labels)
+    test_labels = torch.from_numpy(test_labels)
 
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    # save the normalized data and labels to the save directory
+    torch.save((train_data, train_labels), os.path.join(output_filepath, 'train_set.pt'))
+    torch.save((test_data, test_labels), os.path.join(output_filepath, 'test_set.pt'))
 
+    
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
